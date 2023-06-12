c6be8176da68e0767c4a24ad25f9b4c47c9fda69
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-12 20:26:01 +0600

    popular class update

--- src/Pages/Home/PopularClass/PopularClass.jsx
+++ src/Pages/Home/PopularClass/PopularClass.jsx
@@ -1,48 +1,5 @@
 const PopularClass = ({popularClass}) => {
-    // const classes = [
-    //     {
-    //       "name": "Oil Painting",
-    //       "description": "Learn the art of oil painting and create stunning masterpieces.",
-    //       "category": "popular",
-    //       "image": "https://images.unsplash.com/photo-1578301978693-85fa9c0320b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=719&q=80",
-    //       "students": 50
-    //     },
-    //     {
-    //       "name": "Pottery Making",
-    //       "description": "Experience the joy of creating beautiful pottery with your own hands.",
-    //       "category": "popular",
-    //       "image": "https://images.unsplash.com/photo-1607448885122-b3d4cf451587?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvdHRlcnl8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-    //       "students": 45
-    //     },
-    //     {
-    //       "name": "Watercolor Painting",
-    //       "description": "Explore the art of watercolor painting and unleash your creativity.",
-    //       "category": "popular",
-    //       "image": "https://plus.unsplash.com/premium_photo-1673514503017-86f2bb8975c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-    //       "students": 40
-    //     },
-    //     {
-    //       "name": "Sculpture Making",
-    //       "description": "Learn the techniques of sculpting and bring your ideas to life.",
-    //       "category": "popular",
-    //       "image": "https://plus.unsplash.com/premium_photo-1677171381887-ab01451bb046?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNjdWxwdG9yfGVufDB8fDB8fHww&w=1000&q=80",
-    //       "students": 38
-    //     },
-    //     {
-    //       "name": "Calligraphy",
-    //       "description": "Discover the art of beautiful handwriting and calligraphic designs.",
-    //       "category": "popular",
-    //       "image": "https://images.unsplash.com/photo-1576437660573-a33d2a2f93d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-    //       "students": 35
-    //     },
-    //     {
-    //       "name": "Glassblowing",
-    //       "description": "Experience the ancient art of glassblowing and create unique glass pieces.",
-    //       "category": "popular",
-    //       "image": "https://images.unsplash.com/photo-1483639130939-150975af84e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2xhc3MlMjBibG93aW5nfGVufDB8fDB8fHww&w=1000&q=80",
-    //       "students": 32
-    //     }
-    //   ];
+    
 
 
 
@@ -54,10 +11,10 @@ const PopularClass = ({popularClass}) => {
                     {popularClass?.map((item, i) => (
                         <div className="card-body border rounded-xl" key={i}>
                             <figure className="h-72">
-                                <img src={item.image} alt={item.image} className="object-cover w-full h-full" />
+                                <img src={item.classImage} alt='class' className="object-cover w-full h-full" />
                             </figure>
-                            <h3 className="text-2xl font-bold">{item.name}</h3>
-                            <p className="text-gray-700 font-semibold">{item.description}</p>
+                            <h3 className="text-2xl font-bold">{item.className}</h3>
+                            <p className="text-gray-700 font-semibold">Instructor: {item.name}</p>
                         </div>
                     ))}
                 </div>
