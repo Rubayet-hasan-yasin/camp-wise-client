1e5cec9f96db81a713be94c7028cdb55c124f6c2
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-12 08:55:34 +0600

    approved class

--- src/Pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
@@ -1,8 +1,76 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { toast } from "react-hot-toast";
 
 const ManageClasses = () => {
+    const [axiosSecure] = useAxiosSecure();
+
+
+    const { data: allClasses = [], refetch } = useQuery({
+        queryKey: ['allClasses'],
+        queryFn: async () => {
+            const res = await axiosSecure('/all-classes');
+
+            return res.data;
+        }
+    })
+
+const handleApproved = id =>{
+    
+    axiosSecure.put(`/update-class-status/${id}`)
+    .then(res=>{
+        refetch()
+        if(res.data?.modifiedCount){
+            toast.success('class approved')
+        }
+    })
+}
+
+
+
     return (
-        <div>
-            manage classes
+        <div className="w-[80vw] flex flex-col items-center gap-10">
+            {
+                allClasses.map(classs => (
+                    <div key={classs._id} className="indicator">
+                        <span className="indicator-item badge badge-primary">{classs.status}</span>
+                        <div className="grid w-full place-items-center">
+                            {/* content start */}
+                            <div className="card lg:card-side w-[60vw] bg-base-100 dark:bg-transparent dark:border shadow-xl border-2">
+                                <figure className="w-1/3">
+                                    <img src={classs.classImage} alt="class image" className="object-cover h-full" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title text-3xl font-bold">{classs.className}</h2>
+                                    <p>Available seats: {classs.availableSeats}</p>
+                                    <p>Total Students: {classs.students}</p>
+                                    <p>Price: ${classs.price}</p>
+
+                                    {/* TODO: feedbak from admin */}
+
+                                    <div className="card-actions justify-end">
+                                        <button
+                                        onClick={()=>handleApproved(classs._id)}
+                                            disabled={classs.status === 'approved'}
+                                            className="btn btn-primary"
+                                        >Approve</button>
+
+
+                                        <button 
+                                        disabled={classs.status === 'approved' || classs.status === 'decline'}
+                                        className="btn"
+                                        >Decline and feedback</button>
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+                            {/* content end */}
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
 };
--- src/Pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const ManageUsers = () => {
+    const [axiosSecure] = useAxiosSecure();
+
+    // const {data} = useQuery({
+    //     queryKey: ['']
+    // })
+
+
     return (
         <div>
             manage users
