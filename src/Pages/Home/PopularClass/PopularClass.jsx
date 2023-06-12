52717e29c37abb0afac2ae6cb88e977f689529ca
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-12 23:21:01 +0600

    add animation pak

--- src/Pages/Home/PopularClass/PopularClass.jsx
+++ src/Pages/Home/PopularClass/PopularClass.jsx
@@ -1,5 +1,9 @@
-const PopularClass = ({popularClass}) => {
-    
+import { motion } from "framer-motion"
+
+
+
+const PopularClass = ({ popularClass }) => {
+
 
 
 
@@ -9,13 +13,24 @@ const PopularClass = ({popularClass}) => {
                 <h2 className="text-5xl font-bold mb-16 text-center">Popular Art and Craft Classes</h2>
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
                     {popularClass?.map((item, i) => (
-                        <div className="card-body border rounded-xl" key={i}>
-                            <figure className="h-72">
-                                <img src={item.classImage} alt='class' className="object-cover w-full h-full" />
-                            </figure>
-                            <h3 className="text-2xl font-bold">{item.className}</h3>
-                            <p className="text-gray-700 font-semibold">Instructor: {item.name}</p>
-                        </div>
+                        <motion.div
+                        key={i}
+                            initial={{ scale: 0 }}
+                            animate={{ rotate: 360, scale: 1 }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 260,
+                                damping: 20
+                            }}
+                        >
+                            <div className="card-body border rounded-xl" >
+                                <figure className="h-72">
+                                    <img src={item.classImage} alt='class' className="object-cover w-full h-full" />
+                                </figure>
+                                <h3 className="text-2xl font-bold">{item.className}</h3>
+                                <p className="text-gray-700 font-semibold">Instructor: {item.name}</p>
+                            </div>
+                        </motion.div>
                     ))}
                 </div>
             </div>
